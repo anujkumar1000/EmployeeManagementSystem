8d5c6637300502bf352dd52289d23ab488777485
anujkumar1000 <78134072+anujkumar1000@users.noreply.github.com>
2024-10-03 14:21:35 +0530

    Delete db-schema-jooq directory

--- db-schema-jooq/build/resources/main/db/migration/V0_0_2__seed_temp.sql
+++
@@ -1,7 +0,0 @@
-insert into temp
-    (`code`, `name`, `description`, `created_by`, `modified_by`)
-values
-    ('101', 'T101', 'description T101', 'seed', 'seed'),
-    ('102', 'T102', 'description T102', 'seed', 'seed'),
-    ('103', 'T103', 'description T103', 'seed', 'seed'),
-    ('104', 'T104', 'description T104', 'seed', 'seed');
